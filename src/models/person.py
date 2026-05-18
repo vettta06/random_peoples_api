8d6ab2a244e522e1987d672650c11063981834fd
Liza <kuznecovaElizaveta2006@yandex.ru>
2026-05-18 19:26:06 +0300

    configure alembic migrations

---
+++ src/models/person.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from src.db.session import Base
+
+
+class Person(Base):
+    """Модель пользователя."""
+
+    __tablename__ = "people"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    gender: Mapped[str] = mapped_column(String(20), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    phone: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    city: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+    )
