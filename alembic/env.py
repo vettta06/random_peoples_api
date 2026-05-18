231d4d7389f866ab87e4ba87ab348c7c90adfdb3
Liza <kuznecovaElizaveta2006@yandex.ru>
2026-05-18 22:01:47 +0300

    update settings for ci

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
-
 from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from src.core.config import config as app_config
 from src.db.session import Base
 
 config = context.config
@@ -16,35 +18,42 @@ target_metadata = Base.metadata
 def run_migrations_offline() -> None:
     """Запустить миграции без подключения к БД."""
 
-    url = config.get_main_option("sqlalchemy.url")
+    url = app_config.database_url
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
+
     with context.begin_transaction():
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Запустить миграции."""
+def do_run_migrations(connection) -> None:  # type: ignore
+    """Запустить миграции внутри синхронного контекста."""
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
     )
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-        )
-        with context.begin_transaction():
-            context.run_migrations()
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Запустить миграции в асинхронном режиме."""
+
+    connectable = create_async_engine(app_config.database_url)
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
 
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    asyncio.run(run_migrations_online())
